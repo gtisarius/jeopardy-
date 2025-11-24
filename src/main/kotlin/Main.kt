e1c168732711fa505dc2e11aae5e149b38fa36ad
dkienenb <dkienenb@gmail.com>
2025-11-24 16:11:17 -0500

    Basic server infrastructure

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+import io.ktor.http.ContentType
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.http.content.default
+import io.ktor.server.http.content.files
+import io.ktor.server.http.content.static
+import io.ktor.server.http.content.staticFiles
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import java.io.File
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            staticFiles("/", File("src/main/resources/webroot")) {
+                default("index.html")
+            }
+        }
+    }.start(wait = true)
+}

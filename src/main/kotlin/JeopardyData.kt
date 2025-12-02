5b2851d9fd519414f3df86069d940d8030ec8ff2
dkienenb <dkienenb@gmail.com>
2025-12-02 12:20:42 -0500

    add choose question endpoint and a lot of server util functions

--- src/main/kotlin/JeopardyData.kt
+++ src/main/kotlin/JeopardyData.kt
@@ -21,11 +21,48 @@ enum class Difficulty {
 }
 
 data class GameInstance(val players: List<String>, val categories: Set<Category>) {
+
+    val questionsToPoints: Map<Category, Map<Int, Question>>
+    val answeredQuestions = mutableListOf<Question>()
+    var currentQuestion: Question? = null
+
     init {
         if (players.size < 2 || players.size > categories.size * 5) {
-            throw IllegalArgumentException("bad number of players")
+            throw IllegalArgumentException("Bad number of players")
         }
+        val newMap = mutableMapOf<Category, Map<Int, Question>>()
+        categories.forEach { category ->
+            val questionsToPoints = mutableMapOf<Int, Question>()
+            val questions = category.questions
+            val q100 = questions.filter { it.difficulty == Difficulty.EASY }.shuffled().first()
+            questionsToPoints[100] = q100
+            val q200 = questions.filter { it.difficulty == Difficulty.EASY || it.difficulty == Difficulty.MEDIUM}.shuffled().first()
+            questionsToPoints[200] = q200
+            val q300 = questions.filter { it.difficulty == Difficulty.MEDIUM }.shuffled().first()
+            questionsToPoints[300] = q300
+            val q400 = questions.filter { it.difficulty == Difficulty.MEDIUM || it.difficulty == Difficulty.HARD}.shuffled().first()
+            questionsToPoints[400] = q400
+            val q500 = questions.filter { it.difficulty == Difficulty.HARD }.shuffled().first()
+            questionsToPoints[500] = q500
+            newMap[category] = questionsToPoints
+        }
+        this.questionsToPoints = newMap
     }
+
+
     @OptIn(ExperimentalTime::class)
     val gameId: String = System.now().toString()
+
+    fun fetchQuestion(category: String, pointValue: Int) {
+        val selected = categories.firstOrNull { it.name == category }
+        if (selected == null) {
+            throw KotlinNullPointerException("No such category")
+        }
+        val selectedQuestion = questionsToPoints[selected]?.get(pointValue)
+            ?: throw KotlinNullPointerException("No question with that point value")
+        if (answeredQuestions.contains(selectedQuestion)) {
+            throw IllegalArgumentException("Question already answered")
+        }
+        currentQuestion = selectedQuestion
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,7 +7,11 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import io.ktor.http.ContentType
 import java.io.File
+import java.lang.IllegalArgumentException
+import kotlin.reflect.KClass
 
 fun main() {
     embeddedServer(Netty, port = 12345, module = Application::module).start(wait = true)
@@ -16,88 +20,62 @@ fun main() {
 val categories = mutableSetOf(
     Category("Math", listOf(
         Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
-    )),
-    Category("Math", listOf(
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
+        Question("1+2", Difficulty.EASY,"2"),
+        Question("1+3", Difficulty.MEDIUM,"2"),
+        Question("1+4", Difficulty.MEDIUM,"2"),
+        Question("1+5", Difficulty.MEDIUM,"2"),
+        Question("1+6", Difficulty.HARD,"2"),
+        Question("1+7", Difficulty.HARD,"2"),
     )),
     Category("Math 2", listOf(
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
+        Question("2+1", Difficulty.EASY,"2"),
+        Question("2+2", Difficulty.EASY,"2"),
+        Question("2+3", Difficulty.MEDIUM,"2"),
+        Question("2+4", Difficulty.MEDIUM,"2"),
+        Question("2+5", Difficulty.MEDIUM,"2"),
+        Question("2+6", Difficulty.HARD,"2"),
+        Question("2+7", Difficulty.HARD,"2"),
     )),
     Category("Math 3", listOf(
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
+        Question("3+1", Difficulty.EASY,"2"),
+        Question("3+2", Difficulty.EASY,"2"),
+        Question("3+3", Difficulty.MEDIUM,"2"),
+        Question("3+4", Difficulty.MEDIUM,"2"),
+        Question("3+5", Difficulty.MEDIUM,"2"),
+        Question("3+6", Difficulty.HARD,"2"),
+        Question("3+7", Difficulty.HARD,"2"),
     )),
     Category("Math 4", listOf(
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
+        Question("4+1", Difficulty.EASY,"2"),
+        Question("4+2", Difficulty.EASY,"2"),
+        Question("4+3", Difficulty.MEDIUM,"2"),
+        Question("4+4", Difficulty.MEDIUM,"2"),
+        Question("4+5", Difficulty.MEDIUM,"2"),
+        Question("4+6", Difficulty.HARD,"2"),
+        Question("4+7", Difficulty.HARD,"2"),
     )),
     Category("Math 5", listOf(
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
+        Question("5+1", Difficulty.EASY,"2"),
+        Question("5+2", Difficulty.EASY,"2"),
+        Question("5+3", Difficulty.MEDIUM,"2"),
+        Question("5+4", Difficulty.MEDIUM,"2"),
+        Question("5+5", Difficulty.MEDIUM,"2"),
+        Question("5+6", Difficulty.HARD,"2"),
+        Question("5+7", Difficulty.HARD,"2"),
     )),
     Category("Math 6", listOf(
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
-    )),
-    Category("Math 7", listOf(
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
-    )),
-    Category("Math 8", listOf(
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.EASY,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.MEDIUM,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
-        Question("1+1", Difficulty.HARD,"2"),
+        Question("6+1", Difficulty.EASY,"2"),
+        Question("6+2", Difficulty.EASY,"2"),
+        Question("6+3", Difficulty.MEDIUM,"2"),
+        Question("6+4", Difficulty.MEDIUM,"2"),
+        Question("6+5", Difficulty.MEDIUM,"2"),
+        Question("6+6", Difficulty.HARD,"2"),
+        Question("6+7", Difficulty.HARD,"2"),
     )),
 )
 
 val games = mutableListOf<GameInstance>()
+val gson = Gson()
 
 fun Application.module() {
     routing {
@@ -106,20 +84,61 @@ fun Application.module() {
         }
         route("api") {
             post("/start_game") {
-                val gson = Gson()
-                val postData = call.receiveText()
-                val gameStartRequest = gson.fromJson(postData, GameStartRequest::class.java)
+                val gameStartRequest = call.parseAs(GameStartRequest::class)
                 val categoryCount = gameStartRequest.categoryCount?:5
-                val categoryNames = categories.shuffled().take(categoryCount).toSet()
-                val game = GameInstance(gameStartRequest.players, categoryNames)
+                val applicableCategories = categories.shuffled().take(categoryCount)
+                if (applicableCategories.size != categoryCount) {
+                    call.respondError("Not enough categories")
+                }
+                val categoryNames = applicableCategories.toSet()
+                val game = try {
+                    GameInstance(gameStartRequest.players, categoryNames)
+                } catch (e: Exception) {
+                    call.respondError(e.message ?: "Error creating game")
+                    return@post
+                }
                 games.add(game)
                 val gameStartResponse = GameStartResponse(game.gameId, game.categories.map { it.name }.toSet())
-                val responseMessage = gson.toJson(gameStartResponse)
-                call.respondText(responseMessage, ContentType.Application.Json, HttpStatusCode.OK)
+                call.respondJson(gameStartResponse)
             }
             post("/choose_question") {
-
+                val chooseQuestionRequest = call.parseAs(ChooseQuestionRequest::class)
+                val game = getGameById(chooseQuestionRequest.gameId, call)
+                if (game != null) {
+                    game.fetchQuestion(chooseQuestionRequest.category, chooseQuestionRequest.pointValue)
+                    val question = game.currentQuestion!!
+                    val response = ChooseQuestionResponse(question.text, chooseQuestionRequest.pointValue)
+                    call.respondJson(response)
+                }
             }
         }
     }
+}
+
+private suspend fun getGameById(gameId: String, call: RoutingCall) : GameInstance? {
+    val game = games.firstOrNull {it.gameId == gameId}
+    if (game == null) {
+        call.respondError("No game with that id found")
+    }
+    return game
+}
+
+private suspend fun <T : Any> RoutingCall.parseAs(clazz: KClass<T>) : T {
+    val postData = receiveText()
+    try {
+        val thing = gson.fromJson(postData, clazz.java)
+        return thing
+    } catch (e: JsonSyntaxException) {
+        respondError("Could not parse request")
+        throw IllegalArgumentException("Improperly formatted request")
+    }
+}
+
+private suspend fun RoutingCall.respondError(error: String) {
+    respondJson(ErrorResponse("$error!"), HttpStatusCode.InternalServerError)
+}
+
+private suspend fun RoutingCall.respondJson(value: Any, statusCode: HttpStatusCode = HttpStatusCode.OK) {
+    val json = gson.toJson(value)
+    respondText(json, ContentType.Application.Json, statusCode)
 }
